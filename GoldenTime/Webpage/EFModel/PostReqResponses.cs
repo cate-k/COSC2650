58fd231404643db8251df71001416c9c195d046f
Andrew <aceware152@gmail.com>
2022-04-04 13:48:16 +1000

    messages added to db, basic page shell added as well

--- GoldenTime/Webpage/EFModel/PostReqResponses.cs
+++ GoldenTime/Webpage/EFModel/PostReqResponses.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 #nullable disable
 
@@ -7,10 +8,16 @@ namespace Webpage.EFModel
 {
     public partial class PostReqResponses
     {
+        [DisplayName("Post ID")]
         public int Idx { get; set; }
         public int PostIdx { get; set; }
+        [DisplayName("Email Address")]
         public int ResponderIdx { get; set; }
+
+        [DisplayName("Date/Time")]
         public DateTime RespondedOn { get; set; }
+
+        [DisplayName("Message")]
         public string ResponseText { get; set; }
         public int ResponseTypeIdx { get; set; }
 
