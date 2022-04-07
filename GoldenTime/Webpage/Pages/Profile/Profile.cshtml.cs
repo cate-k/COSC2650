e8022ba8d22c7cae57d22688fd8822f262cb409c
haryfish <s3559087@student.rmit.edu.au>
2022-04-07 16:27:49 +1000

    testing commit error
    for andrew

--- GoldenTime/Webpage/Pages/Profile/Profile.cshtml.cs
+++ GoldenTime/Webpage/Pages/Profile/Profile.cshtml.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Webpage.EFModel;
+using Webpage.Shared;
 
 namespace Webpage.Pages.Profile
 {
     public class ProfileModel : PageModel
     {
+        private readonly IDbContextFactory<cosc2650Context> _contextFactory;
+        public string Identity;
+
+        public List<POCO.User> Users;
+
+        public ProfileModel(IDbContextFactory<cosc2650Context> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+
+
         public void OnGet()
         {
+            Identity = User.Identity.Name;
+            Users = Helper.GetUsers(_contextFactory);
         }
+
+
+
+
+
     }
 }
